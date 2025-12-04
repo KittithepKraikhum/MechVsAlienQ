2f813f941ad57c6ebeeb48b305109f1ad27c3857
kittithep <kittithepkraikhum@gmail.com>
2025-12-04 17:39:53 +0000

    alien.h and .c

--- alien.c
+++ alien.c
@@ -1,1 +1,13 @@
 //alien.c
+
+#include "alien.h"
+
+
+//function definition
+void InitAlien(Alien * a)
+{
+	a->health = 100;	//Starting HP
+	a->state = ALIEN_IDLE;	//Starting in idle state
+	a->pos = (Vector2){600,300};	//Position on screen
+
+}
--- alien.h
+++ alien.h
@@ -1,1 +1,26 @@
 //alien.h
+
+#ifndef ALIEN_H
+#define ALIEN_H
+
+#include "raylib.h"
+#include "player.h"
+
+//FSM States
+typedef enum {
+	ALIEN_IDLE, //possible player action mode
+	ALIEN_ATTACK,
+	ALIEN_SHIELD,
+	ALIEN_MAGIC
+} AlienState;
+
+typedef struct {
+	int health;
+	AlienState state;
+	Vector2 pos;
+
+} Alien;
+
+void InitAlien(Alien * a);
+
+#endif
