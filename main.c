d89b1cabd2480b61250331e8066f4483191ec975
kittithep <kittithepkraikhum@gmail.com>
2025-12-04 21:16:06 +0000

    add basic game loop and window setup

--- main.c
+++ main.c
@@ -1,1 +1,32 @@
 //main.c
+
+#include "raylib.h"
+#include "player.h"
+#include "alien.h"
+
+int main(void)
+{
+	//create window
+	InitWindow(800,600,"Mech vs Alien");
+	
+	//Declare player and alien
+	Player player;
+	Alien alien;
+
+	//Initialize Player and Alien
+	InitPlayer(&player);
+	InitAlien(&alien);
+
+	//Game loop
+	while (!WindowShouldClose())
+	{
+	//drawing code
+	BeginDrawing();
+	ClearBackground(RAYWHITE); //color the entire screen to white
+
+	EndDrawing();
+
+	}
+
+return 0;
+}//end main
